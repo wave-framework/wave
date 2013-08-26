f650fdddaed8944808b8d408ff5d0e04f44ff976
Patrick Hindmarsh <patrick@hindmar.sh>
2013-08-27 09:15:58 +1200

    Moved exception logging into the try-catch
    No more 'Exception thrown without stack frame at line 0'

--- src/Wave/Exception.php
+++ src/Wave/Exception.php
@@ -41,11 +41,11 @@ class Exception extends \Exception {
 	}
 	
 	public static function handle(\Exception $e){
-		$log_message = sprintf('%-4s %s', "({$e->getCode()})", $e->getMessage());
-		// get the channel manually so the introspection works properly.
-		Log::getChannel('exception')->addRecord(Log::ERROR, $log_message);
-
         try {
+            $log_message = sprintf('%-4s %s', "({$e->getCode()})", $e->getMessage());
+            // get the channel manually so the introspection works properly.
+            Log::getChannel('exception')->addRecord(Log::ERROR, $log_message);
+
             $request = static::$request;
             if($request === null)
                 $request = Request::createFromGlobals();
