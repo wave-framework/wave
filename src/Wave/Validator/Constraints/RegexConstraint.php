63af722d15b1c041d528ac611323df4a07769f6e
Patrick Hindmarsh <patrick@hindmar.sh>
2013-11-18 10:26:14 +1300

    Allow custom messages for regex constraints

--- src/Wave/Validator/Constraints/RegexConstraint.php
+++ src/Wave/Validator/Constraints/RegexConstraint.php
@@ -2,7 +2,24 @@
 
 namespace Wave\Validator\Constraints;
 
+use Wave\Validator;
+
 class RegexConstraint extends AbstractConstraint {
+
+    private $message;
+
+    public function __construct($property, $arguments, Validator &$validator){
+        parent::__construct($property, $arguments, $validator);
+
+        if(is_array($arguments) && !is_callable($arguments)){
+            if(isset($arguments['message'], $arguments['pattern'])){
+                $this->message = $arguments['message'];
+                $this->arguments = $arguments['pattern'];
+            }
+            else throw new \InvalidArgumentException("Invalid format for regex constraint, must contain a [message] and [pattern]");
+        }
+
+    }
 
     /**
      * Evaluate the current constraint against the schema arguments and input data.
@@ -11,6 +28,13 @@ class RegexConstraint extends AbstractConstraint {
      */
     public function evaluate() {
         return preg_match($this->arguments, $this->data) > 0;
+    }
+
+    protected function getViolationMessage($context = 'This value'){
+        if(isset($this->message)){
+            return sprintf($this->message, $context);
+        }
+        else return parent::getViolationMessage($context);
     }
 
 }
