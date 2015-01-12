8afd399c844e6fbc57b88941efb687e5243214d4
Patrick Hindmarsh <patrick@hindmar.sh>
2015-01-13 09:34:53 +1300

    Allow matching on multiple properties in exists constraint
     - All schema values being matched must be validated first
     - Multiple properties are specified in the format [ model_property => schema_property ] where the schema_property is already validated and will be matched against the model_property in the query (as an = comparison).

--- src/Wave/Validator/Constraints/ExistsConstraint.php
+++ src/Wave/Validator/Constraints/ExistsConstraint.php
@@ -15,6 +15,7 @@ class ExistsConstraint extends AbstractConstraint implements CleanerInterface {
 
     protected $type = 'exists';
     protected $message = null;
+    private $match_fields = array();
     private $instance = null;
 
     public function __construct($property, $arguments, Validator &$validator){
@@ -23,16 +24,29 @@ class ExistsConstraint extends AbstractConstraint implements CleanerInterface {
 
         parent::__construct($property, $arguments, $validator);
 
+        $this->match_fields = $arguments['property'];
+        if(!is_array($arguments['property'])){
+            $this->match_fields = array($arguments['property'] => $property);
+        }
+
+        if(empty($this->match_fields))
+            throw new \InvalidArgumentException("[$this->type] constraint requires at least one property to match");
+
         $this->message = isset($arguments['message']) ? $arguments['message'] : null;
-        $this->instance = DB::get()->from($this->arguments['model'])
-            ->where($this->arguments['property'] . ' = ?', $this->data)
-            ->fetchRow();
     }
 
     /**
      * @return bool
      */
     public function evaluate(){
+        $statement = DB::get()->from($this->arguments['model']);
+
+        foreach($this->match_fields as $column => $input_key) {
+            $statement->where($column . ' = ?', $this->validator[$input_key]);
+        }
+
+        $this->instance = $statement->fetchRow();
+
         return $this->instance instanceof Model;
     }
 
