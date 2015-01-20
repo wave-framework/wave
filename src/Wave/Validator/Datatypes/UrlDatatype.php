215e92e3dad9ae2a10ea6768c302abafe5d89977
Patrick Hindmarsh <patrick@hindmar.sh>
2015-01-21 09:24:57 +1300

    Added UrlDatatype

---
+++ src/Wave/Validator/Datatypes/UrlDatatype.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Wave\Validator\Datatypes;
+
+class UrlDatatype extends AbstractDatatype {
+
+    private $cleaned;
+
+	public function __invoke(){
+        $this->cleaned = trim($this->input);
+		return filter_var($this->cleaned, FILTER_VALIDATE_URL);
+	}
+
+    public function getCleanedData(){
+        return $this->cleaned;
+    }
+
+    /**
+     * @return string a type to use in the violation message
+     */
+    public function getType() {
+        return 'url';
+    }
+}
