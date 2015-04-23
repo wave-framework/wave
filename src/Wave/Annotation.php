19c8f69ace052d2e1094ff82d8541415d1263d17
Michael Calcinai <michael@calcin.ai>
2015-04-24 11:56:31 +1200

    Updated annotation regex to be more picky with multiline annotations

--- src/Wave/Annotation.php
+++ src/Wave/Annotation.php
@@ -5,11 +5,11 @@ namespace Wave;
 use Wave\Annotation\InvalidAnnotationException;
 
 class Annotation {
-	
-	const FOR_CLASS = 'class';
-	
-	const CLASS_CONTROLLER = '\\Wave\\Controller';
-	const CLASS_MODEL = '\\Wave\\Model';
+
+    const FOR_CLASS = 'class';
+
+    const CLASS_CONTROLLER = '\\Wave\\Controller';
+    const CLASS_MODEL = '\\Wave\\Model';
 
     protected $key;
     protected $value;
@@ -34,24 +34,24 @@ class Annotation {
         return new $class($key, $value, $from_class);
     }
 
-	public static function parse($block, $originating_class){
+    public static function parse($block, $originating_class){
 
         if(empty($block)) return array();
 
         $block = self::sanitizeDocBlock($block);
 
         $annotations = array();
-        $pattern = '/(?<=(?:\@|\~))([a-zA-Z\_\-\\\][a-zA-Z0-9\_\-\.\\\]*)(((?!\s(?:\@|\~)).)*)/s';
+        $pattern = '/^[~@](?<annotation>\w+)(?:[\t\f ](?<arguments>.+?(?=\s[~@\s])))?/ms';
         preg_match_all($pattern, $block, $found);
-        foreach ($found[2] as $key => $value) {
-            $type = strtolower($found[1][$key]);
-            $value = trim($value);
-            $annotations[] = self::factory($type, $value, $originating_class);
-        }
 
+        foreach ($found['annotation'] as $position => $annotation) {
+            $arguments = $found['arguments'][$position];
+            $type = strtolower($annotation);
+            $annotations[] = self::factory($type, $arguments, $originating_class);
+        }
         return $annotations;
 
-	}
+    }
 
     /**
      * Remove comment notation (/ and *) from a raw docblock
@@ -60,16 +60,16 @@ class Annotation {
      * @return mixed
      */
     protected static function sanitizeDocBlock($docblock){
-        return preg_replace('/^(\s*\*+\s{0,1}\/?)|(\/\*{1,2})/m', '', $docblock);
+        return preg_replace('/^([\t\f ]*\*[\t\f ]+)/m', '', $docblock);
     }
 
-	public function __construct($key, $value, $from_class = null) {
+    public function __construct($key, $value, $from_class = null) {
         $this->key = $key;
         $this->value = $value;
         $this->from_class = $from_class;
-	}
+    }
 
-	public function apply(Router\Action &$action){}
+    public function apply(Router\Action &$action){}
 
     /**
      * @return mixed
@@ -86,10 +86,10 @@ class Annotation {
     }
 
     protected function validOnSubclassesOf($annotatedClass, $baseClass) {
-		if( $annotatedClass != $baseClass && !is_subclass_of($annotatedClass, $baseClass) )
-			throw new InvalidAnnotationException(get_class($this) . " is only valid on objects of type {$baseClass}.");
+        if( $annotatedClass != $baseClass && !is_subclass_of($annotatedClass, $baseClass) )
+            throw new InvalidAnnotationException(get_class($this) . " is only valid on objects of type {$baseClass}.");
 
-	}
+    }
 
 
 }
