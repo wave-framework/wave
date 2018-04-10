25e1fa2fe1bf635efdb2dfb7613e18889b45dc24
Michael Calcinai <michael@calcin.ai>
2018-04-10 16:24:45 +1200

    Allow array or object values in JSON column

--- src/Wave/DB/Driver/AbstractDriver.php
+++ src/Wave/DB/Driver/AbstractDriver.php
@@ -4,16 +4,17 @@ namespace Wave\DB\Driver;
 
 use Wave\DB\Column;
 
-abstract class AbstractDriver {
+abstract class AbstractDriver
+{
 
-    public static function valueToSQL($value) {
-
-        switch(true) {
+    public static function valueToSQL($value)
+    {
+        switch (true) {
             case is_bool($value):
                 return $value ? 1 : 0;
             case $value instanceof \DateTimeInterface:
                 return $value->format('Y-m-d H:i:s');
-            case is_object($value):
+            case is_object($value) || is_array($value):
                 return json_encode($value);
 
             default:
@@ -21,34 +22,38 @@ abstract class AbstractDriver {
         }
     }
 
-    public static function valueFromSQL($value, array $field_data) {
-
-        if($value === null)
+    public static function valueFromSQL($value, array $field_data)
+    {
+        if ($value === null) {
             return null;
-        else if(!is_scalar($value))
-            return $value;
+        } else {
+            if (!is_scalar($value)) {
+                return $value;
+            }
+        }
 
-        switch($field_data['data_type']) {
+        switch ($field_data['data_type']) {
 
             case Column::TYPE_BOOL:
-                return (bool) $value;
+                return (bool)$value;
 
             case Column::TYPE_INT:
-                return (int) $value;
+                return (int)$value;
 
             case Column::TYPE_FLOAT:
-                return (float) $value;
+                return (float)$value;
 
             case Column::TYPE_STRING:
-                return (string) $value;
+                return (string)$value;
 
             case Column::TYPE_JSON:
                 return json_decode($value);
 
             case Column::TYPE_DATE:
             case Column::TYPE_TIMESTAMP:
-                if($value == 'CURRENT_TIMESTAMP')
+                if ($value == 'CURRENT_TIMESTAMP') {
                     $value = 'now';
+                }
                 return new \DateTime($value);
 
             default:
