a55e9727bcac56cb3b541f0ae4a4c25e1a5e7ee8
Matt Renner <matt@rennernz.com>
2022-08-29 15:56:31 +1200

    Ensure the generated flag is set for stored or virtual columns

--- src/Wave/DB/Column.php
+++ src/Wave/DB/Column.php
@@ -175,7 +175,7 @@ class Column {
      */
     public function isGenerated(): bool
     {
-        return stripos($this->getExtra(), 'VIRTUAL') === 0;
+        return stripos($this->getExtra(), 'VIRTUAL') === 0 || stripos($this->getExtra(), 'STORED') === 0;
     }
 
     private function parseMetadata($raw) {
